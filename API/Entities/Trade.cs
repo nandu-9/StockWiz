60ed985ea3fd64510cdff25ef3b8e1ce10d4e691
Nandakumar Abbigeri <officialnandu@gmail.com>
2023-01-16 16:32:01 +0530

    Added Trade Entity and dropped boiler plate code

---
+++ API/Entities/Trade.cs
@@ -0,0 +1,13 @@
+﻿namespace API.Entities
+{
+    public class Trade
+    {
+        public int Id { get; set; }
+        public string CompanySym { get; set; }
+        public int Volume { get; set; }
+        public DateTime TimeStamp { get; set; }
+
+        // Navigation properties
+        public string UserName { get; set; }
+    }
+}
